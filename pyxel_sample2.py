14bfdea9a83efe39613a503f8e6563154cc2fd72
hhama <hhama@notus.dti.ne.jp>
2025-07-08 19:05:43 +0900

    ひとまずプログラムを変えてみた

--- pyxel_sample2.py
+++ pyxel_sample2.py
@@ -4,6 +4,8 @@ import pyxel
 class TextApp:
     def __init__(self):
         pyxel.init(160, 120, title="All Colors")
+        self.count = 0
+        self.old_list = pyxel.colors.to_list()
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -11,12 +13,17 @@ class TextApp:
             pyxel.quit()
 
     def draw(self):
-        pyxel.cls(7)
-        pyxel_table = [hex(x) for x in pyxel.colors.to_list()]
-        y = 3
-        for idx, color in enumerate(pyxel_table):
-            pyxel.text(55, y, color, idx)
-            y += 7
+        if pyxel.frame_count % 100 == 0:
+            y = 3
+            pyxel.cls(7)
+            self.count += 1
+            new_list = []
+            for idx in range(16):
+                new_list.append(self.old_list[(self.count + idx) % 16])
+            pyxel_table = [hex(x) for x in new_list]
+            for idx, color in enumerate(pyxel_table):
+                pyxel.text(55, y, color, idx)
+                y += 7
 
 
 TextApp()
